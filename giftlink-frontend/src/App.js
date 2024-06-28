f61a8c1471d8fa9003ce22b9be80b530740510a2
vickstormtechnology <vickstorm.technology@gmail.com>
2024-06-28 18:32:32 -0400

    first commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
