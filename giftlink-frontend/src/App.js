0cd30d76dff2f22e2d4b674eb668c6f0a207a3d7
vickstormtechnology <vickstorm.technology@gmail.com>
2024-06-28 15:00:28 -0400

    front end first commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
@@ -14,6 +15,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
